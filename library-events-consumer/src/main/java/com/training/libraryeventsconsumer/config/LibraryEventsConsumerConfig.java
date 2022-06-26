daee981123d50d1329b987a18ce557c5eb77c998
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2022-06-26 20:00:44 +0200

    separating modules

--- library-events-consumer/src/main/java/com/training/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/training/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,20 +1,56 @@
 package com.training.libraryeventsconsumer.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.List;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class LibraryEventsConsumerConfig {
 
-    /**
-     * public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-     *                                                                                        ConsumerFactory<Object, Object> kafkaConsumerFactory) {
-     *         final ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-     *         configurer.configure(factory, kafkaConsumerFactory);
-     *         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-     *
-     *         return factory;
-     *     }
-     */
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+                                                                                       ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+
+        final ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+
+        // To set the Acknowledgment of Events manually
+        // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        //factory.setConcurrency(1);
+        factory.setCommonErrorHandler(createErrorHandler());
+
+        return factory;
+    }
+
+    public DefaultErrorHandler createErrorHandler() {
+        // with delay of one second
+        FixedBackOff fixedBackOff = new FixedBackOff(1000L, 2);
+
+        DefaultErrorHandler defaultErrorHandler = new DefaultErrorHandler(fixedBackOff);
+        getExceptionsToIgnoreList().forEach(defaultErrorHandler::addNotRetryableExceptions);
+        defaultErrorHandler.setRetryListeners(((consumerRecord, error, deliverAttempt) -> {
+            log.info("Failed Record in Retry Listener, Exception: {}, deliveryAttempt: {}", error.getMessage(), deliverAttempt);
+        }));
+
+        return defaultErrorHandler;
+    }
+
+    private List getExceptionsToIgnoreList() {
+
+        return List.of(
+                IllegalArgumentException.class
+        );
+    }
+
 }
--- library-events-consumer/src/test/java/com/training/libraryeventsconsumer/consumer/LibraryEventsConsumerIntegrationTest.java
+++ library-events-consumer/src/test/java/com/training/libraryeventsconsumer/consumer/LibraryEventsConsumerIntegrationTest.java
@@ -1,12 +1,15 @@
 package com.training.libraryeventsconsumer.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.training.libraryeventsconsumer.entity.Book;
 import com.training.libraryeventsconsumer.entity.LibraryEvent;
 import com.training.libraryeventsconsumer.repository.LibraryEventRepository;
 import com.training.libraryeventsconsumer.service.LibraryEventService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,21 +40,27 @@ import static org.mockito.Mockito.verify;
 class LibraryEventsConsumerIntegrationTest {
 
     @Autowired
-    EmbeddedKafkaBroker embeddedKafkaBroker;
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
 
     @Autowired
-    KafkaTemplate<Integer, String> kafkaTemplate;
+    private KafkaTemplate<Integer, String> kafkaTemplate;
 
     @Autowired
-    KafkaListenerEndpointRegistry endpointRegistry;
+    private KafkaListenerEndpointRegistry endpointRegistry;
 
     @Autowired
-    LibraryEventRepository libraryEventRepository;
+    private LibraryEventRepository libraryEventRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @SpyBean
-    LibraryEventService libraryEventServiceSpy;
+    private LibraryEventService libraryEventServiceSpy;
 
     @SpyBean
-    LibraryEventsConsumer libraryEventsConsumerSpy;
+    private LibraryEventsConsumer libraryEventsConsumerSpy;
+
+    private CountDownLatch countDownLatch;
 
 
     @BeforeEach
@@ -59,6 +68,7 @@ class LibraryEventsConsumerIntegrationTest {
         for (MessageListenerContainer messageListenerContainer : endpointRegistry.getListenerContainers()) {
             ContainerTestUtils.waitForAssignment(messageListenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
         }
+        countDownLatch = new CountDownLatch(1);
     }
 
     @AfterEach
@@ -74,7 +84,7 @@ class LibraryEventsConsumerIntegrationTest {
         // Test
         // making the call asynchronous
         kafkaTemplate.sendDefault(jsonString).get();
-        CountDownLatch countDownLatch = new CountDownLatch(1);
+
         countDownLatch.await(3, TimeUnit.SECONDS);
 
         // Verify
@@ -87,5 +97,36 @@ class LibraryEventsConsumerIntegrationTest {
             assert libraryEvent.getLibraryEventId() != null;
             assertThat(libraryEvent.getBook().getBookId(), is(967));
         });
+    }
+
+    @Test
+    @Disabled
+    void publishUpdateLibraryEvent() throws JsonProcessingException, InterruptedException {
+        // Prepare
+        final String jsonString = "{\n    \"libraryEventId\": 967,\n    \"libraryEventType\": \"NEW\",\n    \"book\": {\n        \"bookId\": 967,\n        \"bookName\": \"Kafka Using Spring Boot 2\",\n        \"bookAuthor\": \"Greatness Stephane\"\n    }\n}";
+        final LibraryEvent libraryEvent = objectMapper.readValue(jsonString, LibraryEvent.class);
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+
+        libraryEventRepository.save(libraryEvent);
+        String updatedBookName = "Kafka Using Spring Boot 2.x";
+        Book updatedBook = Book.builder()
+                .bookId(libraryEvent.getBook().getBookId())
+                .bookName(updatedBookName)
+                .bookAuthor(libraryEvent.getBook().getBookAuthor())
+                .build();
+        libraryEvent.setBook(updatedBook);
+
+        // Test
+        final String updatedJsonString = objectMapper.writeValueAsString(libraryEvent);
+        kafkaTemplate.sendDefault(libraryEvent.getLibraryEventId(), updatedJsonString);
+
+        countDownLatch.await(3, TimeUnit.SECONDS);
+
+        // Verify
+        verify(libraryEventsConsumerSpy, times(1)).onMessage(isA(ConsumerRecord.class));
+        verify(libraryEventServiceSpy, times(1)).processLibraryEvent(isA(ConsumerRecord.class));
+        //final LibraryEvent persistedLibraryEvent = libraryEventRepository.findById(1).get();
+        //assertThat(persistedLibraryEvent.getBook().getBookName(), is(updatedBookName));
+
     }
 }
