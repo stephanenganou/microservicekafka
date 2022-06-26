d12e8108b59d19d72191e7ba6f900f07cbfeec65
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2022-06-26 13:15:41 +0200

    edits first integrationstest for consumer test

--- library-events-consumer/src/test/java/com/training/libraryeventsconsumer/consumer/LibraryEventsConsumerIntegrationTest.java
+++ library-events-consumer/src/test/java/com/training/libraryeventsconsumer/consumer/LibraryEventsConsumerIntegrationTest.java
@@ -1,8 +1,11 @@
 package com.training.libraryeventsconsumer.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.training.libraryeventsconsumer.entity.LibraryEvent;
+import com.training.libraryeventsconsumer.repository.LibraryEventRepository;
 import com.training.libraryeventsconsumer.service.LibraryEventService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +19,13 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -39,11 +45,14 @@ class LibraryEventsConsumerIntegrationTest {
     @Autowired
     KafkaListenerEndpointRegistry endpointRegistry;
 
+    @Autowired
+    LibraryEventRepository libraryEventRepository;
     @SpyBean
     LibraryEventService libraryEventServiceSpy;
 
     @SpyBean
     LibraryEventsConsumer libraryEventsConsumerSpy;
+
 
     @BeforeEach
     void setUp() {
@@ -52,10 +61,15 @@ class LibraryEventsConsumerIntegrationTest {
         }
     }
 
+    @AfterEach
+    void tearDown() {
+        libraryEventRepository.deleteAll();
+    }
+
     @Test
     void publishNewLibraryEvent() throws ExecutionException, InterruptedException, JsonProcessingException {
         // Prepare
-        final String jsonString = "{\n    \"libraryEventId\": 967,\n    \"libraryEventType\": \"NEW\",\n    \"book\": {\n        \"bookId\": 600,\n        \"bookName\": \"Kafka Using Spring Boot 2\",\n        \"bookAuthor\": \"Greatness Stephane\"\n    }\n}";
+        final String jsonString = "{\n    \"libraryEventId\": 967,\n    \"libraryEventType\": \"NEW\",\n    \"book\": {\n        \"bookId\": 967,\n        \"bookName\": \"Kafka Using Spring Boot 2\",\n        \"bookAuthor\": \"Greatness Stephane\"\n    }\n}";
 
         // Test
         // making the call asynchronous
@@ -66,5 +80,12 @@ class LibraryEventsConsumerIntegrationTest {
         // Verify
         verify(libraryEventsConsumerSpy, times(1)).onMessage(isA(ConsumerRecord.class));
         verify(libraryEventServiceSpy, times(1)).processLibraryEvent(isA(ConsumerRecord.class));
+
+        List<LibraryEvent> libraryEventList = (List<LibraryEvent>) libraryEventRepository.findAll();
+        assertThat(libraryEventList.size(), is(1));
+        libraryEventList.forEach(libraryEvent -> {
+            assert libraryEvent.getLibraryEventId() != null;
+            assertThat(libraryEvent.getBook().getBookId(), is(967));
+        });
     }
 }
